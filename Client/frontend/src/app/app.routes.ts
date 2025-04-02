9d0a81e951e9c36ec3d335f463c383b6e0a0a846
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-04-02 01:53:34 -0600

    Update routing to set LoginComponent as the default route

--- Client/frontend/src/app/app.routes.ts
+++ Client/frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '', component: LoginComponent },
+  { path: '**', redirectTo: '' }
 ];
