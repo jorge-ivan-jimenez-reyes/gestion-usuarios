225013b8916677415e340e41418cc67cd2ec6536
jorge-ivan-jimenez-reyes <0251139@up.edu.mx>
2025-04-01 21:24:02 -0600

    Your commit message here

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -3,7 +3,11 @@ const { Pool } = require('pg');
 class Database {
   constructor() {
     this.pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
     });
   }
 
